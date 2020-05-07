d7596e4d2859bc51c3f5419337b62c893d27d820
Nuria <c.odena.nuria@gmail.com>
2020-05-07 15:15:40 +0200

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
 
+    serverId: number = 10;
+    serverStatus: string = 'offLine';
+
+    getServerStatus () {
+        return this.serverStatus
+    }
+
 }
